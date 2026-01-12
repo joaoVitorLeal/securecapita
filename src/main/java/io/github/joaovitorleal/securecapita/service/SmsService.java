d09ee1fbbfa7e790f7fb2253db8b05bc3ba0312c
joaoVitorLeal <joaoleal98@outlook.com>
2026-01-12 16:33:40 -0300

    refactor: complete architecture cleanup

--- src/main/java/io/github/joaovitorleal/securecapita/service/SmsService.java
+++ src/main/java/io/github/joaovitorleal/securecapita/service/SmsService.java
@@ -3,20 +3,31 @@ package io.github.joaovitorleal.securecapita.service;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service("smsService")
 public class SmsService implements NotificationService {
-    public static final String FROM_NUMBER = "71988960906";
-    public static final String TWILIO_ACCOUNT_SID = "";
-    public static final String TWILIO_AUTH_TOKEN = "";
+
+    @Value("${FROM_NUMBER}")
+    private String fromNumber;
+
+    @Value("${TWILIO_ACCOUNT_SID}")
+    private String twilioAccountSid;
+
+    @Value("${TWILIO_AUTH_TOKEN}")
+    private String twilioAuthToken;
 
     /**
      * @param to Phone number recipient
      * @param message body of the message
      */
     public void sendMessage(String to, String message) {
-        Twilio.init(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-        Message smsMessage = Message.creator(new PhoneNumber("+55" + to),  new PhoneNumber("+55" + FROM_NUMBER), message ).create();
+        Twilio.init(twilioAccountSid, twilioAuthToken);
+        Message smsMessage = Message.creator(
+                new PhoneNumber("+55" + to),
+                new PhoneNumber("+55" + fromNumber),
+                message
+        ).create();
     }
 }
