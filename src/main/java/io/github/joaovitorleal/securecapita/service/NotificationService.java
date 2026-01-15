00abdd4b3623b47cd0c5ab412248150f7eaeed9b
joaoVitorLeal <joaoleal98@outlook.com>
2026-01-15 11:17:31 -0300

    feat: add NotificationService#sendMfaCode

--- src/main/java/io/github/joaovitorleal/securecapita/service/NotificationService.java
+++ src/main/java/io/github/joaovitorleal/securecapita/service/NotificationService.java
@@ -3,4 +3,5 @@ package io.github.joaovitorleal.securecapita.service;
 public interface NotificationService {
 
     void sendMessage(String to, String message);
+    void sendMfaCode(String userFirstName, String to, String mfaCode);
 }
