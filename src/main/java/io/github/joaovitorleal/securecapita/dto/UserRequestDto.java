40e7e36eceb4d5831ca4c45d056ebe3eaf6efcfa
joaoVitorLeal <joaoleal98@outlook.com>
2026-01-15 11:43:33 -0300

    refactor: modify validation annotations

--- src/main/java/io/github/joaovitorleal/securecapita/dto/UserRequestDto.java
+++ src/main/java/io/github/joaovitorleal/securecapita/dto/UserRequestDto.java
@@ -1,19 +1,20 @@
 package io.github.joaovitorleal.securecapita.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 
 public record UserRequestDto(
-        @NotEmpty(message = "First name must not be empty.")
+        @NotBlank(message = "First name must not be empty.")
         String firstName,
 
-        @NotEmpty(message = "Last name must not be empty.")
+        @NotBlank(message = "Last name must not be empty.")
         String lastName,
 
         @Email(message = "Invalid E-mail.")
+        @NotBlank(message = "Email cannot be empty.")
         String email,
 
-        @NotEmpty(message = "Password must not be empty.")
+        @NotBlank(message = "Password must not be empty.")
         String password
 ) {
 }
