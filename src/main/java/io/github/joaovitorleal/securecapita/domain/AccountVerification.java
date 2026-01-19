86e6aab7e7609c35d23becf7a2bbe30942967390
joaoVitorLeal <joaoleal98@outlook.com>
2026-01-19 19:52:45 -0300

    refactor: replace Lombok with manual implementation in AccountVerification

--- src/main/java/io/github/joaovitorleal/securecapita/domain/AccountVerification.java
+++ src/main/java/io/github/joaovitorleal/securecapita/domain/AccountVerification.java
@@ -1,24 +1,17 @@
 package io.github.joaovitorleal.securecapita.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "account_verifications")
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class AccountVerification implements Serializable {
 
     @Serial
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 2L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,4 +23,56 @@ public class AccountVerification implements Serializable {
 
     @Column(unique = true, nullable = false)
     private String url;
+
+    public AccountVerification() {}
+
+    public AccountVerification(User user, String url) {
+        this.url = url;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountVerification that = (AccountVerification) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountVerification{" +
+                "id=" + id +
+                ", userId=" + (user != null ? user.getId() : null) +
+                ", url='" + url + '\'' +
+                '}';
+    }
 }
