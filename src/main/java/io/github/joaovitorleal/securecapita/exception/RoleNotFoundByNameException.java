5419ea23dbe090a244cb2b6259dc2079390cfe11
joaoVitorLeal <joaoleal98@outlook.com>
2026-01-19 21:13:04 -0300

    refactor: change RoleNotFoundByNameException superclass to ResourceNotFoundException

--- src/main/java/io/github/joaovitorleal/securecapita/exception/RoleNotFoundByNameException.java
+++ src/main/java/io/github/joaovitorleal/securecapita/exception/RoleNotFoundByNameException.java
@@ -1,6 +1,6 @@
 package io.github.joaovitorleal.securecapita.exception;
 
-public class RoleNotFoundByNameException extends ApiException {
+public class RoleNotFoundByNameException extends ResourceNotFoundException {
 
     public RoleNotFoundByNameException(String roleName) {
         super("Role not found with name: " + roleName);
