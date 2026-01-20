6c2feb8fdfb0f584e4bc77afe3a03ff9fa4d9d5d
joaoVitorLeal <joaoleal98@outlook.com>
2026-01-19 21:07:10 -0300

    refactor: replace Lombok Data/SuperBuilder with explicit methods in MfaVerification

--- src/main/java/io/github/joaovitorleal/securecapita/domain/MfaVerification.java
+++ src/main/java/io/github/joaovitorleal/securecapita/domain/MfaVerification.java
@@ -1,25 +1,18 @@
 package io.github.joaovitorleal.securecapita.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
 @Table(name = "two_factor_verifications")
-@Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class MfaVerification implements Serializable {
 
     @Serial
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 2L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,4 +27,66 @@ public class MfaVerification implements Serializable {
 
     @Column(name = "expiration_date", nullable = false)
     private LocalDateTime expirationDate;
+
+    public MfaVerification() {}
+
+    public MfaVerification(User user, String code, LocalDateTime expirationDate) {
+        this.user = user;
+        this.code = code;
+        this.expirationDate = expirationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public LocalDateTime getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDateTime expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        MfaVerification that = (MfaVerification) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "MfaVerification{" +
+                "id=" + id +
+                ", userId=" + (user != null ? user.getId() : null) +
+                ", code='" + "***" + '\'' +
+                ", expirationDate=" + expirationDate +
+                '}';
+    }
 }
